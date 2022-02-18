22dc3029d97c0e92b2a16858a35fe4c8553cda84
Giulio Zanchetta <zank@outlook.it>
2022-02-19 00:45:48 +0100

    update packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-/**
- * Site scroll
- */
 export { default as GoTopWithProgress } from "./GoTopWithProgress.astro";
-export { default as ScrollStatus } from "./ScrollStatus.astro";
-
-/**
- * Mouse effects
- */
-export { default as MouseBackground } from "./MouseBackground.astro";
